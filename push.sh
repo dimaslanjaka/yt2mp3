18bbefd7bec4bdef5ad874f1480f02ddc0718652
dimaslanjaka <dimaslanjaka@gmail.com>
2019-07-15 16:11:25 +0700

    Initial commit

--- push.sh
+++ push.sh
@@ -3,7 +3,8 @@ git config --global github.token 37b1ce0af59ce0f6dfecaa202eeae4b1728b0e44
 git config --global user.email "dimaslanjaka@gmail.com"
 git config --global user.name "dimaslanjaka"
 git config --global user.signingkey 1DEDA67CD4106FF5
-git config --global credential.helper store
+#git config --global credential.helper store
+git config --global credential.helper wincred
 git remote add origin https://github.com/dimaslanjaka/yt2mp3.git
 git config --global gui.encoding utf-8
 git remote -v
