465202c962de597c43de97886ab283add15e3e5e
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-15 12:51:58 +0700

    disable F drive

--- src/YTDL-batch-file.js
+++ src/YTDL-batch-file.js
@@ -7,7 +7,7 @@ const { default: batch } = require('./YTDL-batch');
 
 // D:/bin/ffmpeg/bin
 let destfolder = path.join(process.cwd(), 'tmp/batch');
-if (fs.existsSync('F:/')) destfolder = 'F:/';
+// if (fs.existsSync('F:/')) destfolder = 'F:/';
 const batchfile = path.join(process.cwd(), 'batch-list.txt');
 const config = {
   dest: destfolder,
