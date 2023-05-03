c7adf9347bd0f4cccf31ca4df4eb030683d49796
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-03 15:05:14 +0700

    feat(feature): add batch text getter

--- runner.cjs
+++ runner.cjs
@@ -1,4 +1,5 @@
 const cp = require('cross-spawn');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 const minimist = require('minimist');
 const { join } = require('path');
 
@@ -7,27 +8,44 @@ const { join } = require('path');
 
 const args = minimist(process.argv.slice(2));
 
-(function main() {
-  const lists = args._;
+(async function main() {
   const saveDir = args['o'] || args['output'];
   if (!saveDir) return console.log('save directory not defined');
 
-  lists.forEach(list => {
-    cp.async(
-      'node',
-      [
-        join(__dirname, 'cli.js'),
-        '--cookie-file',
-        'cookie.txt',
-        '--cache-file',
-        'cache.txt',
-        list,
-      ],
-      {
-        cwd: saveDir,
-        stdio: 'inherit',
-        shell: true,
-      },
-    );
-  });
+  const lists = args._;
+  // get batch.txt lines
+  const batchtxt = join(process.cwd(), 'batch.txt');
+  if (existsSync(batchtxt)) {
+    const batch = readFileSync(batchtxt, 'utf-8')
+      .split(/\r?\n/)
+      .map(str => str.trim())
+      .filter(str => str.length > 0);
+    lists.push(...batch);
+  }
+
+  for (let i = 0; i < lists.length; i++) {
+    const list = lists[i];
+    await cp
+      .async(
+        'node',
+        [
+          join(__dirname, 'cli.js'),
+          '--cookie-file',
+          'cookie.txt',
+          '--cache-file',
+          'cache.txt',
+          list,
+        ],
+        {
+          cwd: saveDir,
+          stdio: 'inherit',
+          shell: true,
+        },
+      )
+      .then(() => {
+        // remove from batch.txt
+        lists.splice(lists.indexOf(list), 1);
+        writeFileSync(batchtxt, lists.join('\n'));
+      });
+  }
 })();
